f4b7aaa1773fd9966913330dc5062c18e3e5ff7e
Paul Yoon <pauljinyoon@gmail.com>
2024-08-23 23:14:43 -0700

    linked ui and base46 to my forked version

--- lua/nvchad/options.lua
+++ lua/nvchad/options.lua
@@ -42,10 +42,6 @@ o.undofile = true
 -- interval for writing swap file to disk, also used by gitsigns
 o.updatetime = 250
 
--- go to previous/next line with h,l,left arrow and right arrow
--- when cursor reaches end/beginning of line
-opt.whichwrap:append "<>[]hl"
-
 -- g.mapleader = " "
 
 -- disable some default providers
--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -2,14 +2,14 @@ return {
     "nvim-lua/plenary.nvim",
 
     {
-        "NvChad/base46",
+        "pauly00n/base46",
         build = function()
             require("base46").load_all_highlights()
         end,
     },
 
     {
-        "NvChad/ui",
+        "pauly00n/ui",
         lazy = false,
         build = function()
             dofile(vim.fn.stdpath "data" .. "/lazy/ui/lua/nvchad_feedback.lua")()
