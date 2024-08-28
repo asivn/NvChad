36bb3767ab58726e9ce32c8a47f27803c8bc84a3
Paul Yoon <pauljinyoon@gmail.com>
2024-08-27 18:46:43 -0700

    autopairs.

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -76,7 +76,18 @@ return {
     dependencies = {
       -- autopairing of (){}[] etc
       {
-        "jiangmiao/auto-pairs",
+        "windwp/nvim-autopairs",
+        opts = {
+          fast_wrap = {},
+          disable_filetype = { "TelescopePrompt", "vim" },
+        },
+        config = function(_, opts)
+          require("nvim-autopairs").setup(opts)
+
+          -- setup cmp for autopairs
+          local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+          require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
       },
 
       -- cmp sources plugins
