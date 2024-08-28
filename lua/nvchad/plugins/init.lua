f24a37b40bbda17f7f39c9aa7d11f04755d75579
Paul Yoon <pauljinyoon@gmail.com>
2024-08-27 18:33:51 -0700

    autopairs?

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -1,174 +1,163 @@
 return {
-    "nvim-lua/plenary.nvim",
+  "nvim-lua/plenary.nvim",
 
-    {
-        "pauly00n/base46",
-        build = function()
-            require("base46").load_all_highlights()
-        end,
+  {
+    "pauly00n/base46",
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+
+  {
+    "pauly00n/ui",
+    lazy = false,
+    build = function()
+      dofile(vim.fn.stdpath "data" .. "/lazy/ui/lua/nvchad_feedback.lua")()
+    end,
+  },
+
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = function()
+      dofile(vim.g.base46_cache .. "devicons")
+      return { override = require "nvchad.icons.devicons" }
+    end,
+  },
+
+  -- file managing , picker etc
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    opts = function()
+      return require "nvchad.configs.nvimtree"
+    end,
+  },
+
+  -- formatting!
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
     },
+  },
 
-    {
-        "pauly00n/ui",
-        lazy = false,
-        build = function()
-            dofile(vim.fn.stdpath "data" .. "/lazy/ui/lua/nvchad_feedback.lua")()
-        end,
+  -- git stuff
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "User FilePost",
+    opts = function()
+      return require "nvchad.configs.gitsigns"
+    end,
+  },
+
+  -- lsp stuff
+  {
+    "williamboman/mason.nvim",
+    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
+    opts = function()
+      return require "nvchad.configs.mason"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    event = "User FilePost",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+    end,
+  },
+
+  -- load luasnips + cmp related in insert mode only
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      -- autopairing of (){}[] etc
+      {
+        "jiangmiao/auto-pairs",
+      },
+
+      -- cmp sources plugins
+      {
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+      },
     },
+    opts = function()
+      return require "nvchad.configs.cmp"
+    end,
+  },
 
-    {
-        "nvim-tree/nvim-web-devicons",
-        opts = function()
-            dofile(vim.g.base46_cache .. "devicons")
-            return { override = require "nvchad.icons.devicons" }
-        end,
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "Telescope",
+    opts = function()
+      return require "nvchad.configs.telescope"
+    end,
+    config = function(_, opts)
+      local telescope = require "telescope"
+      telescope.setup(opts)
+
+      -- load extensions
+      for _, ext in ipairs(opts.extensions_list) do
+        telescope.load_extension(ext)
+      end
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "User FilePost",
+    opts = {
+      indent = { char = "│", highlight = "IblChar" },
+      scope = { char = "│", highlight = "IblScopeChar" },
     },
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "blankline")
 
-    -- file managing , picker etc
-    {
-        "nvim-tree/nvim-tree.lua",
-        cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-        opts = function()
-            return require "nvchad.configs.nvimtree"
-        end,
+      local hooks = require "ibl.hooks"
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+      require("ibl").setup(opts)
+
+      dofile(vim.g.base46_cache .. "blankline")
+    end,
+  },
+
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "User FilePost",
+    opts = {
+      user_default_options = { names = false },
+      filetypes = {
+        "*",
+        "!lazy",
+      },
     },
+    config = function(_, opts)
+      require("colorizer").setup(opts)
 
-    -- formatting!
-    {
-        "stevearc/conform.nvim",
-        opts = {
-            formatters_by_ft = {
-                lua = { "stylua" },
-            },
-        },
-    },
+      -- execute colorizer as soon as possible
+      vim.defer_fn(function()
+        require("colorizer").attach_to_buffer(0)
+      end, 0)
+    end,
+  },
 
-    -- git stuff
-    {
-        "lewis6991/gitsigns.nvim",
-        event = "User FilePost",
-        opts = function()
-            return require "nvchad.configs.gitsigns"
-        end,
-    },
-
-    -- lsp stuff
-    {
-        "williamboman/mason.nvim",
-        cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUpdate" },
-        opts = function()
-            return require "nvchad.configs.mason"
-        end,
-    },
-
-    {
-        "neovim/nvim-lspconfig",
-        event = "User FilePost",
-        config = function()
-            require("nvchad.configs.lspconfig").defaults()
-        end,
-    },
-
-    -- load luasnips + cmp related in insert mode only
-    {
-        "hrsh7th/nvim-cmp",
-        event = { "InsertEnter", "CmdlineEnter" },
-        dependencies = {
-            -- autopairing of (){}[] etc
-            {
-                "windwp/nvim-autopairs",
-                opts = {
-                    fast_wrap = {},
-                    disable_filetype = { "TelescopePrompt", "vim" },
-                },
-                config = function(_, opts)
-                    require("nvim-autopairs").setup(opts)
-
-                    -- setup cmp for autopairs
-                    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
-                    require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
-                end,
-            },
-
-            -- cmp sources plugins
-            {
-                "hrsh7th/cmp-nvim-lua",
-                "hrsh7th/cmp-nvim-lsp",
-                "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-path",
-                "hrsh7th/cmp-cmdline",
-            },
-        },
-        opts = function()
-            return require "nvchad.configs.cmp"
-        end,
-    },
-
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-treesitter/nvim-treesitter" },
-        cmd = "Telescope",
-        opts = function()
-            return require "nvchad.configs.telescope"
-        end,
-        config = function(_, opts)
-            local telescope = require "telescope"
-            telescope.setup(opts)
-
-            -- load extensions
-            for _, ext in ipairs(opts.extensions_list) do
-                telescope.load_extension(ext)
-            end
-        end,
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        event = "User FilePost",
-        opts = {
-            indent = { char = "│", highlight = "IblChar" },
-            scope = { char = "│", highlight = "IblScopeChar" },
-        },
-        config = function(_, opts)
-            dofile(vim.g.base46_cache .. "blankline")
-
-            local hooks = require "ibl.hooks"
-            hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-            require("ibl").setup(opts)
-
-            dofile(vim.g.base46_cache .. "blankline")
-        end,
-    },
-
-    {
-        "NvChad/nvim-colorizer.lua",
-        event = "User FilePost",
-        opts = {
-            user_default_options = { names = false },
-            filetypes = {
-                "*",
-                "!lazy",
-            },
-        },
-        config = function(_, opts)
-            require("colorizer").setup(opts)
-
-            -- execute colorizer as soon as possible
-            vim.defer_fn(function()
-                require("colorizer").attach_to_buffer(0)
-            end, 0)
-        end,
-    },
-
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPost", "BufNewFile" },
-        cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-        build = ":TSUpdate",
-        opts = function()
-            return require "nvchad.configs.treesitter"
-        end,
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-        end,
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    opts = function()
+      return require "nvchad.configs.treesitter"
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
