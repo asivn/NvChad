13dde3b580d5ab57b7ea1eb206aa8775de2a0b1d
Paul Yoon <pauljinyoon@gmail.com>
2024-09-29 13:41:23 -0700

    no indent blankline

--- lua/nvchad/plugins/init.lua
+++ lua/nvchad/plugins/init.lua
@@ -122,25 +122,6 @@ return {
     end,
   },
   {
-    "lukas-reineke/indent-blankline.nvim",
-    enabled = false,
-    event = "User FilePost",
-    opts = {
-      indent = { char = "│", highlight = "IblChar" },
-      scope = { char = "│", highlight = "IblScopeChar" },
-    },
-    config = function(_, opts)
-      dofile(vim.g.base46_cache .. "blankline")
-
-      local hooks = require "ibl.hooks"
-      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-      require("ibl").setup(opts)
-
-      dofile(vim.g.base46_cache .. "blankline")
-    end,
-  },
-
-  {
     "NvChad/nvim-colorizer.lua",
     event = "User FilePost",
     opts = {
